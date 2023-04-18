4bca2b348ecf7528dd08a588ab839f844aff6c42
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-04-18 11:04:11 +0200

    fix if, End

--- js/main.js
+++ js/main.js
@@ -7,22 +7,20 @@ for (let contatore = 1; contatore < 100; contatore++) {
 
     let numero = contatore;
 
-    
-
-    if (numero % 3 == 0) {
-        console.log(`Fizz`);
-        contenitore.innerHTML += `<div class="box box-3">Fizz</div>`;
+    if (numero % 15 == 0) { //Da mettere come prima, perchè se scritta per ultima il ciclo non riesce a raggungerla e non la esegue.
+        console.log(`FizzBuzz`);
+        contenitore.innerHTML += `<div class="box box-S">FizzBuzz</div>`;
 
     } else if (numero % 5 == 0) {
         console.log(`Buzz`);
         contenitore.innerHTML += `<div class="box box-5">Buzz</div>`;
 
-    } else if (numero % 15 == 0) {
-        console.log(`FizzBuzz`);
-        contenitore.innerHTML += `<div class="box box-S">FizzBuzz</div>`;
+    } else if (numero % 3 == 0) {
+        console.log(`Fizz`);
+        contenitore.innerHTML += `<div class="box box-3">Fizz</div>`;
         
     } else {
-        console.log(`numero`)
+        console.log(`Il numero corrente è ` , numero)
         contenitore.innerHTML += `<div class="box">${numero}</div>`;
     }
 }
